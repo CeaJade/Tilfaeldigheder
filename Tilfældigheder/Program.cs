b36afffa353908b797c1aff3c4437f0ad3e54a96
Caspj <Caspj@CASPER>
2021-01-04 11:55:15 +0100

    Added comments

--- Tilfældigheder/Program.cs
+++ Tilfældigheder/Program.cs
@@ -15,21 +15,27 @@ namespace Tilfældigheder
 
             Stopwatch stopwatch = new Stopwatch();
 
+            // Starts the stopwatch and prints 10 random numbers using RNGCryptoServiceProvider
             stopwatch.Start();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 rng.GetBytes(data);
                 int value = BitConverter.ToInt32(data, 0);
                 Console.Write(value + " ");
             }
+            // Stops the stopwatch
             stopwatch.Stop();
 
+            // Prints the amount of milliseconds it took the process to finish
             Console.WriteLine("\nTime: " + stopwatch.ElapsedMilliseconds + " ms\n");
 
-
+            // Set the stopwatch back to 0 and starts it again
             stopwatch.Restart();
-            for (int i = 0; i < 10; i++)
+
+            // Prints 10 random numbers using Random
+            for (int i = 0; i < 1000; i++)
             {
+                rdm.Next(0, 1000000);
                 Console.Write(rdm.Next(0, 1000000) + " "); 
             }
             stopwatch.Stop();
